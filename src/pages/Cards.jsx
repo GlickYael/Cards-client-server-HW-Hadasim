a1b772ec8aaa990be734d1a07c2a0bd34aa55c39
GlickYael <glick.yael3@gmail.com>
2024-10-31 09:46:10 +0200

    Adding search ability

--- src/pages/Cards.jsx
+++ src/pages/Cards.jsx
@@ -2,10 +2,14 @@ import React, { useEffect, useState } from 'react'
 import Card from '../components/Card'
 import services from '../services/CRUD'
 import { FaPlus } from "react-icons/fa";
+import { BsSearchHeart } from "react-icons/bs";
+
 
 
 function Cards() {
     const [cards, setCards] = useState([]);
+    const [search, setSearch] = useState('');
+    const [cardsToShow, setCardsToShow] = useState([]);
     const [colors, setColors] = useState([
         '#698474',
         '#FCF8F3',
@@ -20,10 +24,13 @@ function Cards() {
         fetchData();
     }, [])
 
+    useEffect(() => {
+        handleSearch(search)
+    }, [cards, search]);
     const handleDelete = async (id) => {
         await services.deleteCards(id);
-        let newCards = cards.filter(card => card.id!== id);
-        setCards(newCards)
+        let newCards = cards.filter(card => card.id !== id);
+        setCards(newCards);
     };
 
     const handleAdd = async () => {
@@ -31,20 +38,37 @@ function Cards() {
             text: 'The text comes here!',
             color: colors[Math.floor(Math.random() * (colors.length))]
         }
-        
+
         newCard = await services.createCard(newCard);
         let newCards = [...cards, newCard];
         setCards(newCards);
+        setSearch('');
     }
+
+    const handleSearchChange = async (e) => {
+        setSearch(e.target.value);
+    }
+
+    const handleSearch = async (search) => {
+        let filteredCards = cards.filter(card => card.text.toLowerCase().includes(search.toLowerCase()));
+        setCardsToShow(filteredCards);
+    }
+
     return (
-        <div className='cardsContainer'>
-            {cards.map(card =>
-                <Card key={card.id} card={card} handleDelete={handleDelete} colors={colors}/>
-            )}
-            <div className='addCardsBtn' onClick={handleAdd}>
-                <FaPlus />
+        <>
+            <div class="search-container">
+                <input type="text" value={search} onChange={handleSearchChange} class="search-input" />
+                <BsSearchHeart className='beSearchHeart'/>
             </div>
-        </div>
+            <div className='cardsContainer'>
+                {cardsToShow.map(card =>
+                    <Card key={card.id} card={card} handleDelete={handleDelete} colors={colors} />
+                )}
+                <div className='addCardsBtn' onClick={handleAdd}>
+                    <FaPlus />
+                </div>
+            </div>
+        </>
     )
 }
 
